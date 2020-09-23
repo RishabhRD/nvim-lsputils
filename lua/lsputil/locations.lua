d5a604971fea47044e6e357b15c6fc664bdf4946
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-09-24 02:36:42 +0530

    locations now are opened in right windows

--- lua/lsputil/locations.lua
+++ lua/lsputil/locations.lua
@@ -2,177 +2,184 @@
 -- for parameter references see popfix readme.
 
 local action = require'popfix.action'
+local util = require'lsputil.util'
 
 -- TODO guaranteed to be accesed only by one reference operation by code
 local temp_loc
 
 local popup_buffer = {}
+
+
 local function range_result(range)
-	local line = range.start.line + 1
-	local startLine = range.start.line + 1
-	local endLine
-	if startLine <= 3 then
-		endLine  = 11 - startLine
-		startLine = 1
-	else
-		endLine = startLine + 7
-		startLine = startLine - 2
-	end
-	return {
-		line = line,
-		startLine = startLine,
-		endLine = endLine
-	}
+  local line = range.start.line + 1
+  local startLine = range.start.line + 1
+  local endLine
+  if startLine <= 3 then
+    endLine  = 11 - startLine
+    startLine = 1
+  else
+    endLine = startLine + 7
+    startLine = startLine - 2
+  end
+  return {
+    line = line,
+    startLine = startLine,
+    endLine = endLine
+  }
 end
 
 -- selection handler
 -- returns new preview according to location return by server
 -- when selection is changed in popup
 local function selection_handler(buf, index)
-	local locations = popup_buffer[buf].locations
-	local location = locations[index]
-	local uri = location.uri or location.targetUri
-	local range = location.range or location.targetSelectionRange
-	local range_res = range_result(range)
-	local filePath = vim.uri_to_fname(uri)
-	local raw_command = "cat -n '%s' | sed -n '%s,%sp'"
-	local command = string.format(raw_command, filePath, range_res.startLine,
-		range_res.endLine)
-	local data = vim.fn.systemlist(command)
-	return {
-		data = data,
-		line = range_res.line - range_res.startLine,
-		filetype = popup_buffer[buf].filetype
-	}
+  local locations = popup_buffer[buf].locations
+  local location = locations[index]
+  local uri = location.uri or location.targetUri
+  local range = location.range or location.targetSelectionRange
+  local range_res = range_result(range)
+  local filePath = vim.uri_to_fname(uri)
+  local raw_command = "cat -n '%s' | sed -n '%s,%sp'"
+  local command = string.format(raw_command, filePath, range_res.startLine,
+    range_res.endLine)
+  local data = vim.fn.systemlist(command)
+  return {
+    data = data,
+    line = range_res.line - range_res.startLine,
+    filetype = popup_buffer[buf].filetype
+  }
 end
 
 -- init handler
 -- return preview for the first location return by server
 local function init_handler(_)
-	local location = temp_loc.location
-	local uri = location.uri or location.targetUri
-	local range = location.range or location.targetSelectionRange
-	local range_res = range_result(range)
-	local filePath = vim.uri_to_fname(uri)
-	local raw_command = "cat -n '%s' | sed -n '%s,%sp'"
-	local command = string.format(raw_command, filePath, range_res.startLine,
-		range_res.endLine)
-	local data = vim.fn.systemlist(command)
-	return {
-		data = data,
-		line = range_res.line - range_res.startLine,
-		filetype = temp_loc.filetype
-	}
+  local location = temp_loc.location
+  local uri = location.uri or location.targetUri
+  local range = location.range or location.targetSelectionRange
+  local range_res = range_result(range)
+  local filePath = vim.uri_to_fname(uri)
+  local raw_command = "cat -n '%s' | sed -n '%s,%sp'"
+  local command = string.format(raw_command, filePath, range_res.startLine,
+    range_res.endLine)
+  local data = vim.fn.systemlist(command)
+  return {
+    data = data,
+    line = range_res.line - range_res.startLine,
+    filetype = temp_loc.filetype
+  }
 end
 
 -- close handler
 -- jump to location if line was selection otherwise do nothing
 -- Also cleans the data structure(memory mangement)
 local function close_handler(buf, selected, line)
-	local locations = popup_buffer[buf].locations
-	if selected then
-		vim.lsp.util.jump_to_location(locations[line])
-	end
-	popup_buffer[buf] = nil
+  local locations = popup_buffer[buf].locations
+  if selected then
+    util.jump_to_location(locations[line], popup_buffer[buf].win)
+  end
+  popup_buffer[buf] = nil
 end
 
 local function references_handler(_, _, locations,_,bufnr)
-	if locations == nil or vim.tbl_isempty(locations) then
-		return
-	end
-	local data = {}
-	local filename = vim.api.nvim_buf_get_name(bufnr)
-	for i, location in pairs(locations) do
-		local uri = location.uri or location.targetUri
-		local range = location.range or location.targetSelectionRange
-		local filePath = vim.uri_to_fname(uri)
-		--TODO path shortening
-		local curData = ''..(range.start.line + 1)..' '
-		if filename ~= filePath then
-			curData = curData..filePath .. ': '
-		end
-		local command = "sed '%sq;d' '%s'"
-		command = string.format(command, range.start.line + 1, filePath)
-		local appendedList = vim.fn.systemlist(command)
-		local appendedData = appendedList[1]
-		curData = curData .. appendedData
-		data[i] = curData
-	end
-	local key_maps = {
-		n = {
-			['<CR>'] = action.close_selected,
-			['<ESC>'] = action.close_cancelled,
-		}
-	}
-	local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype')
-	temp_loc = {
-		location = locations[1],
-		filetype = filetype
-	}
-	local buf = require'popfix.preview'.popup_preview(data, key_maps,
-		init_handler, selection_handler, close_handler)
-	popup_buffer[buf] ={
-		locations = locations,
-		filetype = filetype
-	}
-	temp_loc = nil
+  if locations == nil or vim.tbl_isempty(locations) then
+    return
+  end
+  local data = {}
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+  for i, location in pairs(locations) do
+    local uri = location.uri or location.targetUri
+    local range = location.range or location.targetSelectionRange
+    local filePath = vim.uri_to_fname(uri)
+    --TODO path shortening
+    local curData = ''..(range.start.line + 1)..' '
+    if filename ~= filePath then
+      curData = curData..filePath .. ': '
+    end
+    local command = "sed '%sq;d' '%s'"
+    command = string.format(command, range.start.line + 1, filePath)
+    local appendedList = vim.fn.systemlist(command)
+    local appendedData = appendedList[1]
+    curData = curData .. appendedData
+    data[i] = curData
+  end
+  local key_maps = {
+    n = {
+      ['<CR>'] = action.close_selected,
+      ['<ESC>'] = action.close_cancelled,
+    }
+  }
+  local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+  local win = vim.api.nvim_get_current_win();
+  temp_loc = {
+    location = locations[1],
+    filetype = filetype
+  }
+  local buf = require'popfix.preview'.popup_preview(data, key_maps,
+    init_handler, selection_handler, close_handler)
+  popup_buffer[buf] ={
+    locations = locations,
+    filetype = filetype,
+    win = win
+  }
+  temp_loc = nil
 end
 
 
 local definition_handler = function(_,_,locations, _, bufnr)
-	if locations == nil or vim.tbl_isempty(locations) then
-		return
-	end
-	if vim.tbl_islist(locations) then
-		if #locations > 1 then
-			local data = {}
-			local filename = vim.api.nvim_buf_get_name(bufnr)
-			for i, location in pairs(locations) do
-				local uri = location.uri or location.targetUri
-				local range = location.range or location.targetSelectionRange
-				local filePath = vim.uri_to_fname(uri)
-				--TODO path shortening
-				local curData = ''..(range.start.line + 1)..' '
-				if filename ~= filePath then
-					curData = curData..filePath .. ': '
-				end
-				local command = "sed '%sq;d' '%s'"
-				command = string.format(command, range.start.line + 1, filePath)
-				local appendedList = vim.fn.systemlist(command)
-				local appendedData = appendedList[1]
-				curData = curData .. appendedData
-				data[i] = curData
-			end
-			local key_maps = {
-				n = {
-					['<CR>'] = action.close_selected,
-					['<ESC>'] = action.close_cancelled,
-				}
-			}
-			local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype')
-			temp_loc = {
-				location = locations[1],
-				filetype = filetype
-			}
-			local buf = require'popfix.preview'.popup_preview(data, key_maps,
-				init_handler, selection_handler, close_handler)
-			popup_buffer[buf] ={
-				locations = locations,
-				filetype = filetype
-			}
-			temp_loc = nil
-		else
-			vim.lsp.util.jump_to_location(locations[1])
-		end
-	else
-		vim.lsp.util.jump_to_location(locations)
-	end
+  if locations == nil or vim.tbl_isempty(locations) then
+    return
+  end
+  if vim.tbl_islist(locations) then
+    if #locations > 1 then
+      local data = {}
+      local filename = vim.api.nvim_buf_get_name(bufnr)
+      for i, location in pairs(locations) do
+        local uri = location.uri or location.targetUri
+        local range = location.range or location.targetSelectionRange
+        local filePath = vim.uri_to_fname(uri)
+        --TODO path shortening
+        local curData = ''..(range.start.line + 1)..' '
+        if filename ~= filePath then
+          curData = curData..filePath .. ': '
+        end
+        local command = "sed '%sq;d' '%s'"
+        command = string.format(command, range.start.line + 1, filePath)
+        local appendedList = vim.fn.systemlist(command)
+        local appendedData = appendedList[1]
+        curData = curData .. appendedData
+        data[i] = curData
+      end
+      local key_maps = {
+        n = {
+          ['<CR>'] = action.close_selected,
+          ['<ESC>'] = action.close_cancelled,
+        }
+      }
+      local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+      temp_loc = {
+        location = locations[1],
+        filetype = filetype
+      }
+      local win = vim.api.nvim_get_current_win()
+      local buf = require'popfix.preview'.popup_preview(data, key_maps,
+        init_handler, selection_handler, close_handler)
+      popup_buffer[buf] ={
+        locations = locations,
+        filetype = filetype,
+        win = win
+      }
+      temp_loc = nil
+    else
+      vim.lsp.util.jump_to_location(locations[1])
+    end
+  else
+    vim.lsp.util.jump_to_location(locations)
+  end
 end
 
 return{
-	references_handler = references_handler,
-	definition_handler = definition_handler,
-	declaration_handler = definition_handler,
-	typeDefinition_handler = definition_handler,
-	implementation_handler = definition_handler
+  references_handler = references_handler,
+  definition_handler = definition_handler,
+  declaration_handler = definition_handler,
+  typeDefinition_handler = definition_handler,
+  implementation_handler = definition_handler
 }
---
+++ lua/lsputil/util.lua
@@ -0,0 +1,44 @@
+local function get_line_byte_from_position(bufnr, position)
+	print('hello')
+	-- LSP's line and characters are 0-indexed
+	-- Vim's line and columns are 1-indexed
+	local col = position.character
+	-- When on the first character, we can ignore the difference between byte and
+	-- character
+	if col > 0 then
+		local line = position.line
+		local lines = vim.api.nvim_buf_get_lines(bufnr, line, line + 1, false)
+		if #lines > 0 then
+			return vim.str_byteindex(lines[1], col)
+		end
+	end
+	return col
+end
+
+local function jump_to_location(location, win)
+	-- location may be Location or LocationLink
+	local uri = location.uri or location.targetUri
+	if uri == nil then return end
+	local bufnr = vim.uri_to_bufnr(uri)
+	-- Save position in jumplist
+	vim.cmd "normal! m'"
+
+	-- Push a new item into tagstack
+	local from = {vim.fn.bufnr('%'), vim.fn.line('.'), vim.fn.col('.'), 0}
+	local items = {{tagname=vim.fn.expand('<cword>'), from=from}}
+	vim.fn.settagstack(vim.fn.win_getid(), {items=items}, 't')
+
+	--- Jump to new location (adjusting for UTF-16 encoding of characters)
+	vim.api.nvim_set_current_win(win)
+	vim.api.nvim_set_current_buf(bufnr)
+	vim.api.nvim_buf_set_option(0, 'buflisted', true)
+	local range = location.range or location.targetSelectionRange
+	local row = range.start.line
+	local col = get_line_byte_from_position(0, range.start)
+	vim.api.nvim_win_set_cursor(0, {row + 1, col})
+	return true
+end
+
+return{
+	jump_to_location = jump_to_location
+}
